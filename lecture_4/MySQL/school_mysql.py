c294146af4659064e85b337eae94776184139b93
Sabina <sabinaalieva966@gmail.com>
2025-12-04 23:13:39 +0300

    Update school_mysql.py

--- lecture_4/MySQL/school_mysql.py
+++ lecture_4/MySQL/school_mysql.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 config = {
   'user': 'root',
-  'password': "${DB_PASSWORD}",
+  'password': os.getenv('DB_PASSWORD'),
   'host': 'localhost:3306',
   'database': 'inventory',
   'raise_on_warnings': True,
